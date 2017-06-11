2a2a3d9075f896d449bc324980401925ec1564bf
Carlos Medina <carlos.w.med@gmail.com>
2017-06-10 20:10:00 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+var info = {
+	name: 'Carlos Medina',
+	sub_jump: "#signup__input",
+	body_desc1: "With the Internet spreading like wildfire and reaching every part of our daily life, more and more traffic is directed to websites in search for information. Imagine a website without any text and completely overloaded with pictures. That would be too much to bear for us.",
+	body_desc2: 'Many businesses, manufacturers and wholesalers use blogs to promote their products, services. Social interest groups also use blogs for airing their opinion on important issues. Writing catchy blogs is an art as much as developing a website. So how doyou Make Your Blog Popular?',
+	footer_desc: 'There are advances being made in science and technology everyday, and a good example of this is the LCD monitor. LCD monitors have several benefits over the old chunky computer monitors that most users are familiar with. Old computer monitors, take up quite a bit of desktop space, put out a ton of heat, drain a ton of energy, and most often have low picture quality and resolution. Modern advances with the LCD computer monitors have changed a lot of that. The screen itself is flat, and very thin, needing very little desktop space, therefore giving you more room while working at your PC. A big advantage in my opinion is that LCD monitors don’t put off all the heat that the old monitors do.',
+};
+
+var methods = {
+	scroll: function (e) {
+		console.log(e);
+	}
+};
+
+var header = new Vue({
+	el: "#header",
+	data: info,
+	methods: methods
+});
+
+var body = new Vue({
+	el: "#body",
+	data: info
+});
+
+var footer = new Vue({
+	el: "#footer",
+	data: info
+});
---
+++ readme.txt
@@ -0,0 +1,30 @@
+Instructions:
+
+Develop this landing page mockup into a fully functional page with HTML and CSS. 
+Absolutely no UI framework/library (ie. Bootstrap, Semantic, etc.) is to be used.
+Bonus points for using a JS framework like Vue.js or React.js.
+Extra Extra bonus points for connecting to an external JSON dataset for the content!
+
+A jpeg and pdf are provided for reference. Optional PSD and Sketch files are linked below.
+
+Photoshop layout:
+https://drive.google.com/file/d/0B6EQDFZPy9ombTZSTFNaUnluRHc/view?usp=sharing
+Sketch layout:
+https://drive.google.com/file/d/0B6EQDFZPy9omWl9QdEtOdVpmWUE/view?usp=sharing
+
+Assets are extracted and placed in the 'assets' folder.
+
+Use your own picture for the profile picture in the header.
+Replace 'Your Name' with your first and last name.
+"Sign Up Today" button should link to the "Sign up" section near the footer.
+Play Video and Signup field can be dummy fields and empty links.
+Menu is not required for this phase.
+
+Please place all of your source code minus the mockup files into a zip file and email back.
+If you have a hosting platform like Github pages or Heroku, please use that in addition to emailing source. 
+
+Document colors:
+- Primary: #00DFD4,
+- Primary Hover: #009E97,
+- Secondary: #092033,
+- Secondary Hover: #343E45
