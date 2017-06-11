810c4f5b092cab85222be7aaaac4db7034ff150c
Carlos Medina <carlos.w.med@gmail.com>
2017-06-11 17:59:17 -0400

    Set up fetching in the JS file from contentful for info as im still trying to figure out how to send images on a single request.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
-var info = {
-	name: 'Carlos Medina',
-	formValid: false,
-	formInvalid: false,
-	sub_jump: "#signup__input",
-	body_desc1: "With the Internet spreading like wildfire and reaching every part of our daily life, more and more traffic is directed to websites in search for information. Imagine a website without any text and completely overloaded with pictures. That would be too much to bear for us.",
-	body_desc2: 'Many businesses, manufacturers and wholesalers use blogs to promote their products, services. Social interest groups also use blogs for airing their opinion on important issues. Writing catchy blogs is an art as much as developing a website. So how doyou Make Your Blog Popular?',
-	footer_desc: 'There are advances being made in science and technology everyday, and a good example of this is the LCD monitor. LCD monitors have several benefits over the old chunky computer monitors that most users are familiar with. Old computer monitors, take up quite a bit of desktop space, put out a ton of heat, drain a ton of energy, and most often have low picture quality and resolution. Modern advances with the LCD computer monitors have changed a lot of that. The screen itself is flat, and very thin, needing very little desktop space, therefore giving you more room while working at your PC. A big advantage in my opinion is that LCD monitors don’t put off all the heat that the old monitors do.'
-};
+var client = contentful.createClient({
+	// This is the space ID. A space is like a project folder in Contentful terms
+	space: 'gqndkdwyc5hc',
+	// This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+	accessToken: '4c0982aa87f0841b3557f3c07f8bcd4b75a242992dd4991ee3d1ce94a2d399f7'
+});
 
 var methods = {
 	getEmail: function (e) {;
@@ -27,23 +24,39 @@ var methods = {
 	}
 };
 
-var header = new Vue({
-	el: "#header",
-	data: info,
-});
+var setter = function (entry) {
 
-var body = new Vue({
-	el: "#body",
-	data: info
-});
+	var info = {
+		name: entry.fields.data.name,
+		sub_jump: entry.fields.data.sub_jump,
+		formValid: entry.fields.data.formValid,
+		formInvalid: entry.fields.data.formInvalid,
+		body_desc1: entry.fields.data.body_desc1,
+		body_desc2: entry.fields.data.body_desc2,
+		footer_desc: entry.fields.data.footer_desc,
+	}
 
-var signup = new Vue({
-	el: '#sign-up',
-	data: info,
-	methods: methods
-})
+	var header = new Vue({
+		el: "#header",
+		data: info,
+	});
 
-var footer = new Vue({
-	el: "#footer",
-	data: info
-});
+	var body = new Vue({
+		el: "#body",
+		data: info
+	});
+
+	var signup = new Vue({
+		el: '#sign-up',
+		data: info,
+		methods: methods
+	})
+
+	var footer = new Vue({
+		el: "#footer",
+		data: info
+	});
+}
+
+client.getEntry('4LML0PW0FqioGqEYgOO6AQ')
+	.then(setter);
